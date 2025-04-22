094a6a53958aa0dc7bc09296ad67387319be0654
devleandrodias <leandrodbdias@gmail.com>
2025-04-22 17:16:10 -0300

    feat: updated code source

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("Hello, World! Up and running!"))
 	})
 
 	http.ListenAndServe(":8080", nil)
